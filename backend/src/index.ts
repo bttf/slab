166b54c505eee0249c7f1e87814e3118062ea2a5
bttf <adnanchowdhury88@gmail.com>
2022-02-06 20:03:13 -0600

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// hello
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello!!! what goin in hello ok\n");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on port %s", PORT);
+});
