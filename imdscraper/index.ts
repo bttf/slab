2a01ccee8cc380cb84fd44a1a7e720748040f918
bttf <2374625+bttf@users.noreply.github.com>
2022-12-08 08:11:06 -0500

    dockerize

--- imdscraper/index.ts
+++ imdscraper/index.ts
@@ -1,9 +1,45 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import playwright from "playwright";
+
+// TODO https://www.imdb.com/title/tt0082783/parentalguide
+
+async function scrapeGuide(req: Request, res: Response) {
+  const browser = await playwright.chromium.launch({
+    headless: true,
+  });
+
+  console.log("browser created");
+
+  const page = await browser.newPage();
+
+  await page.goto("https://www.imdb.com/title/tt0082783/parentalguide");
+
+  console.log("navigating to page");
+
+  const something = await page.$eval("#advisory-nudity", (headerElm) => {
+    console.log("reading page");
+    const data: any[] = [];
+    const listElms = Array.from(headerElm.getElementsByTagName("li"));
+    listElms.forEach((elm) => {
+      console.log("collecting data...");
+      data.push(elm.innerText.split("\n"));
+    });
+    return data;
+  });
+
+  console.log("Sex & Nudity for 'My Dinner with Andre' --->>>>", something);
+
+  await page.waitForTimeout(5000); // wait
+  await browser.close();
+
+  res.send(something);
+}
 
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  console.log("req received");
+  scrapeGuide(req, res);
 });
 
 app.listen(4200);
